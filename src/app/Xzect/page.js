97f90414268e90d055f24645490e978b9ad0e4d7
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-08-25 11:46:14 +0530

    navbar added with some random pics  and website links

---
+++ src/app/Xzect/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const  Xzect = () => {
+  return (
+    <div>Xzect page</div>
+  )
+}
+
+export default Xzect
---
+++ src/app/components/Nav.js
@@ -0,0 +1,64 @@
+'use client'
+
+import styles from "@/app/styles/navbar.module.css"
+// import {CgCloseR, CgMenu} from "react-icons/cg";
+import Link from "next/link";
+import {useState} from "react";
+
+const Nav = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    console.log("value " + openMenu)
+    return (
+        <>
+            <nav className={styles.navbar}>
+                <div className={openMenu ? `${styles.active}` : "" }>
+                    <ul className={styles.navbarList}>
+                        <li className={styles.navbarItem}>
+                            <Link className={styles.navbarLink} href="https://blog.xzect.com/"
+                                  onClick={() => setOpenMenu(false)}
+                            >Blog</Link>
+                        </li>
+                        <li className={styles.navbarItem}>
+                            <Link className={styles.navbarLink} href="https://www.xzect.com/"
+                                  onClick={() => setOpenMenu(false)}
+                            >Xzect.com</Link>
+                        </li>
+                        <li className={styles.navbarItem}>
+                            <Link className={styles.navbarLink}
+                                  onClick={() => setOpenMenu(false)}
+                                  href="https://courses.xzect.com/all-courses/">Coures</Link>
+                        </li>
+                        <li className={styles.navbarItem}>
+                            <Link className={styles.navbarLink}
+                                  onClick={() => setOpenMenu(false)}
+                                  href="https://xzect.com/contact-us/">Contact</Link>
+                        </li>
+                        <li className={styles.navbarItem}>
+                            <Link className={styles.navbarLink}
+                                  onClick={() => setOpenMenu(false)}
+                                  href="https://courses.xzect.com/dashboard-courses/">Login</Link>
+                        </li>
+                    </ul>
+
+                    {/* //nav icon */}
+                    {/* <div className={styles['mobile-navbar-btn']}>
+                        <CgMenu
+                            name="menu-outline"
+                            className={styles['mobile-nav-icon']}
+                            onClick={() => setOpenMenu(true)}
+                        />
+                        <CgCloseR
+                            name="close-outline"
+                            className={`${styles['mobile-nav-icon']} ${styles['close-outline']}`}
+                            onClick={() => setOpenMenu(false)}
+                        />
+                    </div> */}
+                </div>
+            </nav>
+
+        </>
+
+    );
+};
+
+export default Nav;
